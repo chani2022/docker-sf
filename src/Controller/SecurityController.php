948946691518ead082f9d80bec31d351901c05a2
chani <chaniandrianarinaivo@gmail.com>
2024-10-09 11:22:07 +0200

    auth ok

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    #[Route(path: '/', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils, UserPasswordHasherInterface $hasher, EntityManagerInterface $em): Response
+    {
+        // $user = new User();
+        // $user->setEmail("test@test.com")
+        //      ->setRoles(["ROLE_ADMIN"])
+        //      ->setPassword(
+        //         $hasher->hashPassword($user, "test")
+        //      );
+
+        // $em->persist($user);
+        // $em->flush();
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
